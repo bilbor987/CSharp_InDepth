64b44ad66734f3f11ae4ebdc76ce3b1a0f8a7bf5
Pocol Bogdan <bogdan.pocol@nokia.com>
2020-08-09 13:35:54 +0300

    Add project files.

---
+++ CSharp_InDepth/MainClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_InDepth
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static List<string> GenerateNamesGeneric()
+        {
+            List<string> names = new List<string>();
+            names.Add("Gamma");
+            names.Add("Vlissides");
+            names.Add("Johnson");
+            names.Add("Helm");
+            return names;
+        }
+
+        static void PrintNames(List<string> names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        static ArrayList GenerateNames()
+        {
+            ArrayList names = new ArrayList();
+            names.Add("Gamma");
+            names.Add("Vlissides");
+            names.Add("Johnson");
+            names.Add("Helm");
+            return names;
+        }
+        static void PrintNames(ArrayList names)
+        {
+            foreach (string name in names) 
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
